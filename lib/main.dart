d43c87c09472ff39e8db595dce33a65d07cb4eb0
Naresh Sornapudi <naresh.sornapudi@utdallas.edu>
2021-01-22 21:55:52 -0600

    random shit

--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
 
-      _counter += 2;
+      _counter += 1;
     });
   }
 
